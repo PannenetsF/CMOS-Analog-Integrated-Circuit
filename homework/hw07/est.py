1a784dbd123128983688cf1a5e8c882ac489059c
pannenetsf <3111812673@qq.com>
2020-10-31 14:37:32 +0800

    hw 08

--- homework/hw07/est.py
+++ homework/hw07/est.py
@@ -1,5 +1,7 @@
 import math
 
+
+
 GBWDM = 50e6
 GBWCM = 100e6 * 1.2
 
@@ -16,29 +18,25 @@ VGST3 = 0.2
 VGST4 = 0.2
 VGST7 = 0.2
 
+def est(i, norp):
+    if norp == 'n':
+        return 2 * i / kn * L / (0.2)**2 * 1e6 
+    if norp == 'p':
+        return 2 * i / kp * L / (0.2)**2 * 1e6 
+
 gm1 = 2 * math.pi * CL * GBWDM
 gm7 = 4 * math.pi * CL * GBWCM
 
 # for PMOS 1
 
 I1 = gm1 / 2 * VGST1
-W1 = gm1 * L / kp / VGST1
+W1 = 2 * I1 * L / kp / VGST1**2
 IB1 = 2 * I1
 
 # for NMOS 2
 
 I2 = I1 * 1.1
 W2 = 2 * I2 / kn * L / (VGST2)**2
-
-# for NMOS 3
-
-I3 = I1 * 0.1
-W3 = 2 * I3 / kn * L / (VGST3)**2
-
-# for PMOS 4
-
-I4 = I3
-W4 = 2 * I4 / kp * L / (VGST4)**2
 
 # for NMOS 7
 
@@ -52,8 +50,6 @@ print('I1: uA: ', I1 * 1e6)
 print('gm1: ', gm1)
 print('W1: um: ', W1)
 print('W2: um: ', W2)
-print('W3: um: ', W3)
-print('W4: um: ', W4)
 print('I7: uA: ', I7 * 1e6)
 print('gm7: ', gm7)
 print('W7: um: ', W7)
